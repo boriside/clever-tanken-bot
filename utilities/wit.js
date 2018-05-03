ce221dd6a50b0a9048733a536b7c732b199f1308
DESKTOP-4F082N1\BoIde <boriside@gmail.com>
2018-05-03 19:30:05 +0200

    copied marius code

--- utilities/wit.js
+++ utilities/wit.js
@@ -1,47 +1,44 @@
 const { Wit, log } = require("node-wit")
 
 const WitClient = new Wit({
-  accessToken: "Z2KIGPGGW2NTTR72XFA2GKYZINWNPJ6F",
-  logger: new log.Logger(log.DEBUG)
+    accessToken: "XEIUUVCOS5OKC2MRNCKKMEQI6XTB3FFR",
+    logger: new log.Logger(log.DEBUG)
 })
 
-exports.fetchTextInfo = function(text) {
- 	const result = WitClient.message(text)
-	console.log(result)
-	return result
+exports.fetchTextInfo = function (text) {
+    return WitClient.message(text)
 }
 
-exports.findMatch = function(result, entities, intent) {
+exports.findMatch = function (result, entities, intents) {
 
     let foundIntent = false
-    console.log(intent)
-  if(intent && result.entities && result.entities[intent.name]) {
-      for(intent in intent.intents) {
-        const foundIndex = result.entities.intent.indexOf(intent.value)
-        if(foundIndex != -1) {
-          foundIntent = true
+    if (intents && result.entities && result.entities.intent) {
+        for (intent in intents) {
+            const foundIndex = result.entities.intent.indexOf(intent.value)
+            if (foundIndex != -1) {
+                foundIntent = true
+            }
         }
-      }
-  }
-
-  let values = {}
-  if(entities && result.entities) {
-    for (index in entities) {
-      const entity = entities[index]
-      const value = result.entities[entity]
-      if(value) {
-        values[entity] = value
-      }
     }
-  }
 
-  if((!entities || Object.keys(values).length == entities.length) && (!intent || foundIntent)) { 
-    console.log("values")
-    console.log(values)
-    return values 
-  }
+    let values = {}
+    if (entities && result.entities) {
+        for (index in entities) {
+            const entity = entities[index]
+            const value = result.entities[entity]
+            if (value) {
+                values[entity] = value
+            }
+        }
+    }
 
-  console.log("null")
-  return null
+    if ((!entities || Object.keys(values).length == entities.length) && (!intents || foundIntent)) {
+        console.log("values")
+        console.log(values)
+        return values
+    }
+
+    console.log("null")
+    return null
 
 }
